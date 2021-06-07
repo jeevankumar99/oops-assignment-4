39eaf65256aaf757e50eff80560a722a0ef03543
Jeevan Kumar <rg1kumar1@gmail.com>
2021-06-07 13:39:56 +0530

    Dyamic allocation tested and working!

--- dynamic_2d_array.cpp
+++ dynamic_2d_array.cpp
@@ -1,1 +1,47 @@
 // Illustrate Dynamic Memory allocation using a 2d array and pointers
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int row, col;
+    
+    // get size of rows and columns from user.
+    cout << "Enter the number of rows: ";
+    cin >> row;
+    cout << "Enter the number of columns: ";
+    cin >> col;
+
+    int **parent_array = (int**)malloc(row * sizeof(int*));
+
+    // allocate memory for sub arrays
+    for (int i = 0; i < row; i++)
+    {
+        parent_array[i] = (int*)malloc(col * sizeof(int));
+    }
+
+    // Populate arrays
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << "Enter element (" << i << ", " << j << "): ";
+            cin >> parent_array[i][j];
+        }
+    }
+
+    // Print arrays
+    for (int i = 0; i < row; i++)
+    {
+        cout << "[";
+        for (int j = 0; j < col; j++)
+        {
+            cout << " " << parent_array[i][j] << " ";
+        }
+        cout << "]" << endl;
+    }
+
+    return 1;
+}
